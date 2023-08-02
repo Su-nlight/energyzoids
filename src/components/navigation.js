7ac5334bc29d7d61541aed2fff7fb56b43d07e93
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2023-08-02 14:40:23 +0530

    Delete src/components directory

--- src/components/navigation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './navigation.css'
-
-const Navigation = (props) => {
-  return <header data-role="Header" className="navigation-header"></header>
-}
-
-export default Navigation
